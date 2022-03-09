99c16bbe1c1a4c6703e8a8c23c092abed363971e
aswathselvam <m.aswath08@gmail.com>
2022-03-09 18:38:33 -0500

    Add Histogram Eq

--- Project2/code/lanepredictor.py
+++ Project2/code/lanepredictor.py
@@ -1,32 +1,105 @@
 import cv2
-
-
-import cv2
+import numpy as np
+# import cupy as cp
 
 class LanePredictor:
 
     def __init__(self,data=None,video=None):
         self.data=data
         self.video=video
+        self.HISTOGRAM_EQUALIZATION = "Histogram Equalization"
+        self.ADAPTIVE_HISTOGRAM_EQUALIZATION = "Adaptive Histogram Equalization"
+        self.histogram_profile=[None, None, None]
+        self.USE_HISTOGRAM_MEMORY=False
 
+
+    def histogram(self, im_flat):
+        # RANGE=256
+        # bins = 100
+        # TARGET_BIN_RATIO = (bins//RANGE)
+        bins = 256
+        h = np.zeros(bins)
+        for i in im_flat:
+            h[i] +=1
+        return h
+
+    def cumulate_distribution(self, sequence):
+        bins = []
+        sum=0
+        for i in range(0,len(sequence)):
+            sum+=sequence[i]
+            bins.append(sum)
+        return np.array(bins)
+    
+    def normalize(self, c):
+        c = np.array(c)
+        return ((c - c.min()) * 255) / (c.max() - c.min())
         
-    def histogramEqualization(self,data):
+
+    def adjustGamma(self, image, gamma=1.0):
+        """
+        https://stackoverflow.com/questions/33322488/how-to-change-image-illumination-in-opencv-python
+        
+        """
+        gamma_inv = 1.0 / gamma
+        gammaTable = []
+        for i in np.arange(0, 256):
+            i = (i / 255.0) ** gamma_inv
+            gammaTable.append(i*255)
+        
+        gammaTable = np.array(gammaTable).astype("uint8")
+        
+        # Apply gamma correction using the lookup table
+        return cv2.LUT(image, gammaTable)
+
+    def histogramEqualization(self,data,mode):
         """
         Computes Histogram Equalization
         """
-        self.data=data
-        
-        pass
+        for image in data:
+            cv2.imshow("Original Image", (image))
+
+            if mode==self.HISTOGRAM_EQUALIZATION:
+
+                for i in range(image.shape[2]):
+                    im_channel = image[:,:,i]
+                    flat_image =  im_channel.flatten()
+                    contrast_normalized  = None
+                    if all([x is not None for x in self.histogram_profile]) and self.USE_HISTOGRAM_MEMORY:
+                        contrast_normalized = self.histogram_profile[i]
+                    else:
+                        h = self.histogram(flat_image)
+                        c = self.cumulate_distribution(h)
+                        # c_norm = np.int32(cv2.normalize(c,None, 0,255,cv2.NORM_MINMAX))
+                        contrast_normalized  = np.int32(self.normalize(c))
+                        self.histogram_profile[i]=contrast_normalized                        
+
+                    im_eq = contrast_normalized[flat_image]
+                    im_eq = im_eq.reshape(-1,image.shape[1])
+                    
+                    if i==0:
+                        im_eqs = np.array(im_eq)
+                    else:
+                        im_eqs = np.dstack((im_eqs,im_eq))
+
+                cv2.imshow("Equalilzed Image", np.uint8(im_eqs))
+                cv2.waitKey(50)
+                # return im_eqs
+
+            elif mode==self.ADAPTIVE_HISTOGRAM_EQUALIZATION:
+                im_eqs = self.adjustGamma(image, gamma = 2.0)
+                cv2.imshow("Adaptive Equalilzed Image", np.uint8(im_eqs))
+                cv2.waitKey(50)
 
     def detectStraightLane(self,frame):
         """
         Detects straight Lanes in a given frame
         """
-
+        
         pass
 
 
-    def detectCurvedLane(self):
+    def detectCurvedLane(self,frame):
         """
         Detects Curvatures of Lanes in a given frame
         """
--- Project2/code/solution.py
+++ Project2/code/solution.py
@@ -6,33 +6,41 @@ from scipy.fft import fft, ifft
 import argparse
 from utils import *
 from lanepredictor import LanePredictor
+import glob
 
 def main():
     
     
     Parser = argparse.ArgumentParser()
-    Parser.add_argument('--data_histogramEQ', default='../data/adaptive_hist_data', help='Video path , Default:../data/adaptive_hist_data')
+    Parser.add_argument('--data_histogramEQ', default='../data/adaptive_hist_data/', help='Video path , Default:../data/adaptive_hist_data/')
     Parser.add_argument('--video_whiteline', default='../data/whiteline.mp4', help='Video path , Default:../data/whiteline.mp4')
     Parser.add_argument('--video_challenge', default='../data/challenge.mp4', help='Video path , Default:../data/challenge.mp4')
     Parser.add_argument('--savepath', default='../outputs/', help='Template Path , Default: ../outputs/')
     
     args = Parser.parse_args()
-    data_histogramEQ = args.data_histogramEQ
-    video_whiteline = args.video_whiteline
-    video_challenge = args.video_challenge
+    data_histogramEQ_path = args.data_histogramEQ
+    video_whiteline_path = args.video_whiteline
+    video_challenge_path = args.video_challenge
     savepath = args.savepath
-
-    testudoPath = "../data/testudo.png" 
 
     if(not (os.path.isdir(savepath))):
         os.makedirs(savepath)
+    
+    data_histogramEQ = []
+    for image_file in sorted(glob.glob(data_histogramEQ_path+"*.png")):
+        image = cv2.imread(image_file)
+        image = cv2.resize(image, (image.shape[1]//2,image.shape[0]//2 ) )
+        # cv2.imshow("Histogram equalization data", image)
+        # cv2.waitKey(100)
+        data_histogramEQ.append(image)
 
     #---------Problem 1: Histogram equalization-------------#
     lanePredictor = LanePredictor()
-    lanePredictor.histogramEqualization(data_histogramEQ)
+    lanePredictor.histogramEqualization(data_histogramEQ, mode=lanePredictor.HISTOGRAM_EQUALIZATION)
+    lanePredictor.histogramEqualization(data_histogramEQ, mode=lanePredictor.ADAPTIVE_HISTOGRAM_EQUALIZATION)
 
     out = cv2.VideoWriter('../outputs/'+'whiteline.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 15, (800,640))    
-    cap = cv2.VideoCapture(video_whiteline)
+    cap = cv2.VideoCapture(video_whiteline_path)
 
     while(True):
         ret, frame = cap.read()
@@ -46,7 +54,7 @@ def main():
 
 
     out = cv2.VideoWriter('../outputs/'+'challenge.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 15, (800,640))    
-    cap = cv2.VideoCapture(video_challenge)
+    cap = cv2.VideoCapture(video_challenge_path)
 
     while(True):
         ret, frame = cap.read()
