29ad7fa2f2fff1ac8f416c214def26629e3a7866
aswathselvam <m.aswath08@gmail.com>
2022-03-16 14:27:54 -0400

    Find Projection matrix

--- MidTerm/code/question3.py
+++ MidTerm/code/question3.py
@@ -1,6 +1,5 @@
-from tkinter import W
 import numpy as np
-
+import time 
 """
 Image points |  World points
 x       y    |   X   Y   Z
@@ -19,13 +18,41 @@ world_pts = np.array([[0, 0,0 ],[0, 3, 0],[0,7,0],[0,11,0],[7,1,0],[0,11,7],[7,9
 
 
 img_pts = np.array([[757, 13],[758, 15],[758,86 ],[759,66],[1190,172],[329,1041],[1204,850],[340,59]])
-world_pts = np.array([[0, 0,0 ],[0, 3, 0],[0,7,0],[0,11,0],[7,1,0],[0,11,7],[7,9,0],[0,1,7]])
+world_pts = np.array([[0, 0,0,1 ],[0, 3, 0,1],[0,7,0,1],[0,11,0,1],[7,1,0,1],[0,11,7,1],[7,9,0,1],[0,1,7,1]])
+
+#-----------------P matrix----------------------#
+A = np.empty([3,12],dtype=float)
+O = np.zeros(4,dtype=float)
+for x,X in zip(img_pts,world_pts):
+    u = x[0]
+    v = x[1]
+    w = 1
+    # X = np.asarray(X,dtype=float)
+    # print('X: ',X,' x: ', x,'\n')
+    # i = np.asarray([O,-w*X, v*X])
+    i = np.asarray([np.asarray([O, -w*X, v*X ]).flatten(), 
+                np.asarray([w*X, O, -u*X]).flatten(),
+                np.asarray([-v*X, u*X, O]).flatten()],dtype=float)
+    i = np.squeeze(i)
+    # print("Ashape: ",A.shape, "i : ",i.shape,"\n",i)
+    A=np.vstack((A,i))
+    # print("A\n",A)
+    # input()
+
+print("A: ",A.shape)
+U, D, V = np.linalg.svd(A)
+m, n = A.shape
+A_reconstructed = U[:,:n] @ np.diag(D) @ V[:m,:]
+print("U: ",U.shape)
+print("D: ",D.shape, "\n",  D)
+print("V: ",V.shape, "\n",  V.shape)
+
+print("\nP is in the last column of V:\n",  V[:,n-1])
+p1 = V[0:4,n-1]
+p2 = V[4:8,n-1]
+p3 = V[8:12,n-1]
+
+P = np.array([p1,p2,p3])
+print("\nFinal P matrix: \n",P)
 
 
-def lsqr(A,b):
-	temp = np.matmul(A.T,A)
-	inA = np.linalg.inv(temp)
-	X = np.matmul((inA), np.matmul((A.T),b))
-	return X
-print()
-print(lsqr(world_pts,img_pts).T)
