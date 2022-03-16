4a146f5c4447889521e4183be6bfcc1959704f65
aswathselvam <m.aswath08@gmail.com>
2022-03-15 20:52:19 -0400

    Finish Q1

--- MidTerm/code/question1.py
+++ MidTerm/code/question1.py
@@ -1,9 +1,8 @@
 import cv2
 import numpy as np
 import os.path
-
 from google_drive_downloader import GoogleDriveDownloader as gdd
-
+from utils import Plot, createFolder
 
 
 image_file = './Q1image.png'
@@ -24,10 +23,16 @@ cv2.imshow('Eroded Image', erode_img)
 
 imgray = cv2.cvtColor(erode_img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-cv2.drawContours(image, contours, 0, (0,255,0), 3)
-cv2.imshow('Contours', image)
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+contour_image =  erode_img.copy()
+cv2.drawContours(contour_image, contours, -1, (0,255,0), 3)
+cv2.imshow('Contours', contour_image)
 
 # There are 24 coins in the given image 
-
+print("The number of coins in the image are: ", len(contours))
+plot = Plot(1,3)
+plot.set(image, "Original image")
+plot.set(erode_img, "Eroded image")
+plot.set(contour_image, "Contour image")
+plot.save("../outputs/","output1.png")
 cv2.waitKey(0)
